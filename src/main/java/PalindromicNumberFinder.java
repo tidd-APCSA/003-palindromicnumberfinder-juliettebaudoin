05aeefdac87365e1514844d7a4a0bbdb8d94f289
Juliette Baudoin <637157@aacps.org>
2020-11-11 14:31:52 +0000

    fixed search method

--- src/main/java/PalindromicNumberFinder.java
+++ src/main/java/PalindromicNumberFinder.java
@@ -14,64 +14,23 @@ public class PalindromicNumberFinder {
     public int searchForPalindromicNum(int num){
         Integer myInt = num;
         String myString = myInt.toString();
-        //return myInt + 101;
-        //Integer newNum = myInt + 105;
-        //while (num > 100) {
-          //for (int i = 0; i < myString.length()-1; i++){
-            //System.out.println(myString.charAt((myString.length() + 1) /2) +1 );
-            //System.out.println(num % 10);
-            int newNum = num + 110;
-            // int digit = num % 1000;
-            // System.out.println(digit);
-            // int lastDigit = num % 10;
-            // //System.out.println(digit % 100 - lastDigit);
-            // System.out.println(digit + 10);
-            // // int lastDigit = num % 10;
-            // System.out.println(lastDigit);
-            // System.out.println("new last " +lastDigit);
-            // int middleDigit = num % 100;
-            // System.out.println(middleDigit);
-            // System.out.println("new middle " + (middleDigit + 1));
-            // int firstDigit = num / 100;
-            // System.out.println(firstDigit);
-        //int next = myString.charAt(myString.length()/2) ;
-        //System.out.println(next);
-                         
-  //  int digit = num % 10;
-  //  num /= 10;
-  //  System.out.print(digit + " ");
-//}
-return newNum - num;        //return newNum;
-        //return this.testPalindromicNum(newNum);
+        int i = num;
+        boolean flag = true;
+        while (flag){
+          i++;
+          if(testPalindromicNum(i)){
+            flag = false;
+          }
+        }
+        return i - num;  
     }
+    
 
     // this is a helper method for searchForPalindromicNum. It's purpose is to test if a number is actually a palindrome
     public boolean testPalindromicNum(int num){
         Integer myInt = num;
         String myString = myInt.toString();
-
-        // if (myString.substring(0, myString.length()/2) == myString.substring(myString.length()/2, myString.length())){
-        //   return true;
-        // }
-
-
-        //System.out.println("Int" + myInt);
-        //System.out.println("String" + myString);
-        //System.out.print("reverse" + reverseNum(myInt));
-        //return (this.reverseNum(myInt) == myInt.toString());
-        System.out.println(myInt.toString());
-        // System.out.println(this.reverseNum(myInt));
-        // System.out.println(myInt.toString());
         return myString.equals(this.reverseNum(myInt));
-
-        // for (int i = 0; i < myString.length(); i++){
-        //   return myString()==this.reverseNum(myInt);
-        // }
-        
-
-      // if (myString == myInt.reverseNum()){
-      //   return true;
-      // }
     }
 
 
@@ -82,10 +41,8 @@ return newNum - num;        //return newNum;
         String reverseString = "";
         for (int i = 0; i < myString.length(); i++){
           reverseString = reverseString + myString.charAt(myString.length()-(1+i));
-
         }
         return reverseString;
-
     }
     
 
--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -3,13 +3,9 @@ public class PalindromicRunner {
         // be sure you're testing each method as you work then produce your final output using the toString method
         PalindromicNumberFinder test = new PalindromicNumberFinder(72927);
 
-        //System.out.print(test.reverseNum(400));
-        //System.out.print(myInt.)
-        //System.out.print(myInt);
-        //System.out.println(test.reverseNum(400));
-System.out.println(test.toString());
+      System.out.println(test.toString());
       // System.out.println(test.testPalindromicNum(72927));
-      // System.out.println(test.searchForPalindromicNum(72927));
+    //System.out.println(test.searchForPalindromicNum(72927));
       //System.out.println(test.reverseNum(newNum));
      
       //  System.out.println(test.testPalindromicNum(404));
